24f5591e59a8a48126a4c913558c8697c24807c9
Suraj Mishra <connect@surajmishra.dev>
2025-03-28 19:24:15 +0530

    Add test to default file just to increase test coverage of the project.

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,41 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let controller: AppController;
+  let service: AppService;
+
+  const mockAppService = {
+    getHello: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        {
+          provide: AppService,
+          useValue: mockAppService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+    service = module.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getHello', () => {
+    it('should return the greeting from the service', () => {
+      const greeting = 'Hello World!';
+      mockAppService.getHello.mockReturnValue(greeting);
+      
+      expect(controller.getHello()).toBe(greeting);
+      expect(mockAppService.getHello).toHaveBeenCalled();
+    });
+  });
+});
